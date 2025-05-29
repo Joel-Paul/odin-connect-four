9eccc488e6b76b7d79fc4150b2989c308933a82c
Joel Paul <joel.paul@student.unsw.edu.au>
2025-05-29 16:27:54 +1000

    Add test for empty print_board

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,14 @@
 
 class ConnectFour
+  def print_board
+    puts \
+      "|             |\n" + \
+      "|             |\n" + \
+      "|             |\n" + \
+      "|             |\n" + \
+      "|             |\n" + \
+      "|             |\n" + \
+      "|-------------|\n" + \
+      "|1 2 3 4 5 6 7|"
+  end
 end
-
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,5 +1,22 @@
-
 require_relative '../lib/connect_four'
 
 describe ConnectFour do
+  describe '#print_board' do
+    subject(:new_game) {described_class.new()}
+    context 'when a new game has started' do
+      it 'displays an empty board' do
+        expected = \
+          "|             |\n" + \
+          "|             |\n" + \
+          "|             |\n" + \
+          "|             |\n" + \
+          "|             |\n" + \
+          "|             |\n" + \
+          "|-------------|\n" + \
+          "|1 2 3 4 5 6 7|"
+        expect(new_game).to receive(:puts).with(expected)
+        new_game.print_board
+      end
+    end
+  end
 end
