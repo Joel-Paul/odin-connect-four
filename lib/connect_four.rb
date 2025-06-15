ad559f7e13f38c65427dd1d4097e1a269825bb5f
Joel Paul <joel.v.paul@outlook.com>
2025-06-15 18:33:57 +1000

    Implement get_winner

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,13 +1,14 @@
 
 class ConnectFour
+  attr_reader :board
   def initialize
     @board = Array.new(6) { Array.new(7, :' ') }
   end
 
-  def print_board
+  def print_board(board = @board)
     board_display = '|'
     
-    @board.each do |row|
+    board.each do |row|
       board_display += row.join(' ')
       board_display += "|\n|"
     end
@@ -20,6 +21,14 @@ class ConnectFour
     token = player == 1 ? :o : :x
     row = get_top_row(column - 1)
     @board[row][column - 1] = token
+  end
+
+  def get_winner
+    h_winner = get_horizontal_winner
+    return h_winner if h_winner
+    v_winner = get_vertical_winner
+    return v_winner if v_winner
+    get_diagonal_winner
   end
 
   private
@@ -37,6 +46,52 @@ class ConnectFour
         return i - 1
       end
     end
-    return @board.size - 1
+    @board.size - 1
+  end
+
+  def get_horizontal_winner(board = @board)
+    board.each do |row|
+      row.each_cons(4) do |cons|
+        return 1 if cons.all? { |token| token.to_sym == :o }
+        return 2 if cons.all? { |token| token.to_sym == :x }
+      end
+    end
+    nil
+  end
+
+  def get_vertical_winner(board = @board)
+    get_horizontal_winner board.transpose
+  end
+
+  def get_diagonal_winner(board = @board)
+    down_winner = get_down_diagonal_winner board
+    return down_winner if down_winner
+    get_up_diagonal_winner board
+  end
+
+  def get_down_diagonal_winner(board = @board)
+    diag_board = []
+
+    (board.size - 1).downto 0 do |i|
+      length = board.size - i
+      row = (0...length).collect do |t|
+        board[t + i][t]
+      end
+      row = (0...length).collect { |t| board[t + i][t] }
+      diag_board.append(row)
+      # diag_board.append(row.fill(:' ', row.size...board.size))
+    end
+
+    1.upto(board[0].size - 1) do |j|
+      length = board.size - j + 1
+      row = (0...length).collect { |t| board[t][t + j] }
+      diag_board.append(row)
+    end
+
+    get_horizontal_winner diag_board
+  end
+
+  def get_up_diagonal_winner(board = @board)
+    get_down_diagonal_winner board.map(&:reverse)
   end
 end
